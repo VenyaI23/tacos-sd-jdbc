5111a6dbbca0476a1134412858c0779aaac2636c
VenyaI23 <venom200217@gmail.com>
2023-12-03 22:29:43 +0500

    tacos-sd-jdbc

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,27 @@
+package tacos;
+
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
+
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Table("Taco")
+public class Taco {
+    private long id;
+
+    private Date createdAt = new Date();
+
+    @NotNull
+    @Size(min=5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private List<IngredientRef> ingredients;
+}
---
+++ src/main/java/tacos/TacosSdJdbcApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacosSdJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacosSdJdbcApplication.class, args);
+    }
+
+}
